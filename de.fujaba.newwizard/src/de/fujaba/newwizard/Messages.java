e7eced9acd1bc132614c0a435a607b67d05d1823
chris227 <chris227@upb.de>
2011-06-22 03:42:49 +0000

    share file-> new wizard implementation that allows to add new model elements to the Fujaba model file, editors have to use this dialog in order to serialize their models into one of the categories of the model file (Patch by Ingo Budde)

---
+++ de.fujaba.newwizard/src/de/fujaba/newwizard/Messages.java
@@ -0,0 +1,38 @@
+package de.fujaba.newwizard;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * @generated
+ */
+public class Messages extends NLS {
+
+	/**
+	 * @generated
+	 */
+	static {
+		NLS.initializeMessages("messages", Messages.class); //$NON-NLS-1$
+	}
+
+	/**
+	 * @generated
+	 */
+	private Messages() {
+	}
+
+	public static String ModelElementSelectionPageMessage;
+	public static String NewDiagramFileWizard_RootSelectionPageNoSelectionMessage;
+	public static String NewDiagramFileWizard_RootSelectionPageInvalidSelectionMessage;
+	public static String CreationWizardPageExtensionError;
+	public static String CreationWizard_DiagramModelFilePageTitle;
+	public static String CreationWizard_DiagramModelFilePageDescription;
+	public static String CreationWizardOpenEditorError;
+	public static String CreationWizardCreationError;
+	public static String DiagramEditorUtil_CreateDiagramProgressTask;
+	public static String DiagramEditorUtil_CreateDiagramCommandLabel;
+	public static String NewDiagramFileWizard_CreationPageTitle;
+	public static String NewDiagramFileWizard_CreationPageDescription;
+	public static String AddNewDiagramElementToRootNode;
+
+	// TODO: put accessor fields manually
+}
