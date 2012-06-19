eaa2258e45fcfe88db69d948032860780425daae
aljoschability <aljoschability@googlemail.com>
2012-06-19 10:54:10 +0000

    allowing any expression (instead of only textual expression) on activity edge guard (in gmf label)

--- plugins/org.storydriven.storydiagrams.diagram.custom/src/org/storydriven/storydiagrams/diagram/custom/edit/parts/CustomActivityEdgeGuardConstraintLabelEditPart.java
+++ plugins/org.storydriven.storydiagrams.diagram.custom/src/org/storydriven/storydiagrams/diagram/custom/edit/parts/CustomActivityEdgeGuardConstraintLabelEditPart.java
@@ -4,10 +4,10 @@ import org.eclipse.draw2d.PositionConstants;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.WrappingLabel;
 import org.eclipse.gmf.runtime.notation.View;
-import org.storydriven.core.expressions.TextualExpression;
 import org.storydriven.storydiagrams.activities.ActivityEdge;
 import org.storydriven.storydiagrams.activities.EdgeGuard;
 import org.storydriven.storydiagrams.diagram.custom.util.SdmUtility;
+import org.storydriven.storydiagrams.diagram.custom.util.TextUtil;
 import org.storydriven.storydiagrams.diagram.edit.parts.ActivityEdgeGuardConstraintLabelEditPart;
 
 public class CustomActivityEdgeGuardConstraintLabelEditPart extends ActivityEdgeGuardConstraintLabelEditPart {
@@ -32,13 +32,13 @@ public class CustomActivityEdgeGuardConstraintLabelEditPart extends ActivityEdge
 		ActivityEdge edge = (ActivityEdge) ((View) getModel()).getElement();
 
 		String text = "";
-		if (EdgeGuard.BOOL.equals(edge.getGuard()) && edge.getGuardExpression() != null) {
-			text = format(((TextualExpression) edge.getGuardExpression()).getExpressionText());
+		if (EdgeGuard.BOOL.equals(edge.getGuard())) {
+			text = TextUtil.getText(edge.getGuardExpression());
 		} else if (edge.getGuard() != EdgeGuard.NONE) {
-			text = format(SdmUtility.Guard2String(edge.getGuard()));
+			text = SdmUtility.Guard2String(edge.getGuard());
 		}
 
-		setLabelText(text);
+		setLabelText(format(text));
 	}
 
 	protected static String format(String text) {
