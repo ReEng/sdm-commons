9840fc7dc9ee858ba9e51f8ae348db15b43c10b8
sth0815 <sth0815@googlemail.com>
2012-11-20 20:43:56 +0000

    customized item providers

--- plugins/de.mdelab.sdm.interpreter.core.executionTrace.edit/src-gen/de/mdelab/sdm/interpreter/core/executionTrace/provider/ActivityExecutionItemProvider.java
+++ plugins/de.mdelab.sdm.interpreter.core.executionTrace.edit/src-gen/de/mdelab/sdm/interpreter/core/executionTrace/provider/ActivityExecutionItemProvider.java
@@ -2,19 +2,16 @@
  */
 package de.mdelab.sdm.interpreter.core.executionTrace.provider;
 
-import de.mdelab.sdm.interpreter.core.executionTrace.ActivityExecution;
-import de.mdelab.sdm.interpreter.core.executionTrace.ExecutionTraceFactory;
-import de.mdelab.sdm.interpreter.core.executionTrace.ExecutionTracePackage;
-
 import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
-
+import org.eclipse.emf.edit.EMFEditPlugin;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemColorProvider;
 import org.eclipse.emf.edit.provider.IItemFontProvider;
@@ -28,10 +25,15 @@ import org.eclipse.emf.edit.provider.ITableItemLabelProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.eclipse.emf.edit.provider.ViewerNotification;
 
+import de.mdelab.sdm.interpreter.core.executionTrace.ActivityExecution;
+import de.mdelab.sdm.interpreter.core.executionTrace.ExecutionTraceFactory;
+import de.mdelab.sdm.interpreter.core.executionTrace.ExecutionTracePackage;
+
 /**
- * This is the item provider adapter for a {@link de.mdelab.sdm.interpreter.core.executionTrace.ActivityExecution} object.
- * <!-- begin-user-doc -->
- * <!-- end-user-doc -->
+ * This is the item provider adapter for a
+ * {@link de.mdelab.sdm.interpreter.core.executionTrace.ActivityExecution}
+ * object. <!-- begin-user-doc --> <!-- end-user-doc -->
+ * 
  * @generated
  */
 public class ActivityExecutionItemProvider extends ExecutionItemProvider implements IEditingDomainItemProvider,
@@ -39,174 +41,194 @@ public class ActivityExecutionItemProvider extends ExecutionItemProvider impleme
 		ITableItemColorProvider, ITableItemFontProvider, IItemColorProvider, IItemFontProvider
 {
 	/**
-	 * This constructs an instance from a factory and a notifier.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This constructs an instance from a factory and a notifier. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
-	public ActivityExecutionItemProvider(AdapterFactory adapterFactory)
+	public ActivityExecutionItemProvider(final AdapterFactory adapterFactory)
 	{
 		super(adapterFactory);
 	}
 
 	/**
-	 * This returns the property descriptors for the adapted class.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns the property descriptors for the adapted class. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
-	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object)
+	public List<IItemPropertyDescriptor> getPropertyDescriptors(final Object object)
 	{
-		if (itemPropertyDescriptors == null)
+		if (this.itemPropertyDescriptors == null)
 		{
 			super.getPropertyDescriptors(object);
 
-			addActivityPropertyDescriptor(object);
+			this.addActivityPropertyDescriptor(object);
 		}
-		return itemPropertyDescriptors;
+		return this.itemPropertyDescriptors;
 	}
 
 	/**
-	 * This adds a property descriptor for the Activity feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This adds a property descriptor for the Activity feature. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
-	protected void addActivityPropertyDescriptor(Object object)
+	protected void addActivityPropertyDescriptor(final Object object)
 	{
-		itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
-				getResourceLocator(), getString("_UI_ActivityExecution_activity_feature"),
-				getString("_UI_PropertyDescriptor_description", "_UI_ActivityExecution_activity_feature", "_UI_ActivityExecution_type"),
+		this.itemPropertyDescriptors.add(this.createItemPropertyDescriptor(((ComposeableAdapterFactory) this.adapterFactory)
+				.getRootAdapterFactory(), this.getResourceLocator(), this.getString("_UI_ActivityExecution_activity_feature"), this
+				.getString("_UI_PropertyDescriptor_description", "_UI_ActivityExecution_activity_feature", "_UI_ActivityExecution_type"),
 				ExecutionTracePackage.Literals.ACTIVITY_EXECUTION__ACTIVITY, true, false, true, null, null, null));
 	}
 
 	/**
-	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
-	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
-	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This specifies how to implement {@link #getChildren} and is used to
+	 * deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand},
+	 * {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in
+	 * {@link #createCommand}. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
-	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object)
+	public Collection<? extends EStructuralFeature> getChildrenFeatures(final Object object)
 	{
-		if (childrenFeatures == null)
+		if (this.childrenFeatures == null)
 		{
 			super.getChildrenFeatures(object);
-			childrenFeatures.add(ExecutionTracePackage.Literals.ACTIVITY_EXECUTION__IN_PARAMETER_VALUES);
-			childrenFeatures.add(ExecutionTracePackage.Literals.ACTIVITY_EXECUTION__OUT_PARAMETER_VALUES);
+			this.childrenFeatures.add(ExecutionTracePackage.Literals.ACTIVITY_EXECUTION__IN_PARAMETER_VALUES);
+			this.childrenFeatures.add(ExecutionTracePackage.Literals.ACTIVITY_EXECUTION__OUT_PARAMETER_VALUES);
 		}
-		return childrenFeatures;
+		return this.childrenFeatures;
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
-	protected EStructuralFeature getChildFeature(Object object, Object child)
+	protected EStructuralFeature getChildFeature(final Object object, final Object child)
 	{
-		// Check the type of the specified child object and return the proper feature to use for
+		// Check the type of the specified child object and return the proper
+		// feature to use for
 		// adding (see {@link AddCommand}) it as a child.
 
 		return super.getChildFeature(object, child);
 	}
 
 	/**
-	 * This returns ActivityExecution.gif.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns ActivityExecution.gif. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
-	public Object getImage(Object object)
+	public Object getImage(final Object object)
 	{
-		return overlayImage(object, getResourceLocator().getImage("full/obj16/ActivityExecution"));
+		return this.overlayImage(object, this.getResourceLocator().getImage("full/obj16/ActivityExecution"));
 	}
 
 	/**
-	 * This returns the label text for the adapted class.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
+	 * This returns the label text for the adapted class. <!-- begin-user-doc
+	 * --> <!-- end-user-doc -->
+	 * 
+	 * @generated not
 	 */
 	@Override
-	public String getText(Object object)
+	public String getText(final Object object)
 	{
-		ActivityExecution<?> activityExecution = (ActivityExecution<?>) object;
-		return getString("_UI_ActivityExecution_type") + " " + activityExecution.getExecutionStartedTimeStamp();
+		final ActivityExecution<?> activityExecution = (ActivityExecution<?>) object;
+		String label = this.getString("_UI_ActivityExecution_type");
+
+		label += " " + activityExecution.getExecutionTimeMsec() + " msec - ";
+
+		final ComposedAdapterFactory factory = new ComposedAdapterFactory(EMFEditPlugin.getComposedAdapterFactoryDescriptorRegistry());
+
+		if (activityExecution.getActivity() instanceof EObject)
+		{
+			label += ((IItemLabelProvider) factory.adapt(activityExecution.getActivity(), IItemLabelProvider.class))
+					.getText(activityExecution.getActivity());
+		}
+		else
+		{
+			label += activityExecution.getActivity().toString();
+		}
+
+		return label;
 	}
 
 	/**
-	 * This handles model notifications by calling {@link #updateChildren} to update any cached
-	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This handles model notifications by calling {@link #updateChildren} to
+	 * update any cached children and by creating a viewer notification, which
+	 * it passes to {@link #fireNotifyChanged}. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
-	public void notifyChanged(Notification notification)
+	public void notifyChanged(final Notification notification)
 	{
-		updateChildren(notification);
+		this.updateChildren(notification);
 
 		switch (notification.getFeatureID(ActivityExecution.class))
 		{
 			case ExecutionTracePackage.ACTIVITY_EXECUTION__ACTIVITY:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+				this.fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
 				return;
 			case ExecutionTracePackage.ACTIVITY_EXECUTION__IN_PARAMETER_VALUES:
 			case ExecutionTracePackage.ACTIVITY_EXECUTION__OUT_PARAMETER_VALUES:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+				this.fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
 				return;
 		}
 		super.notifyChanged(notification);
 	}
 
 	/**
-	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
-	 * that can be created under this object.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s
+	 * describing the children that can be created under this object. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
-	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object)
+	protected void collectNewChildDescriptors(final Collection<Object> newChildDescriptors, final Object object)
 	{
 		super.collectNewChildDescriptors(newChildDescriptors, object);
 
-		newChildDescriptors.add(createChildParameter(ExecutionTracePackage.Literals.ACTIVITY_EXECUTION__IN_PARAMETER_VALUES,
+		newChildDescriptors.add(this.createChildParameter(ExecutionTracePackage.Literals.ACTIVITY_EXECUTION__IN_PARAMETER_VALUES,
 				ExecutionTraceFactory.eINSTANCE.create(ExecutionTracePackage.Literals.MAP_ENTRY)));
 
-		newChildDescriptors.add(createChildParameter(ExecutionTracePackage.Literals.ACTIVITY_EXECUTION__OUT_PARAMETER_VALUES,
+		newChildDescriptors.add(this.createChildParameter(ExecutionTracePackage.Literals.ACTIVITY_EXECUTION__OUT_PARAMETER_VALUES,
 				ExecutionTraceFactory.eINSTANCE.create(ExecutionTracePackage.Literals.MAP_ENTRY)));
 	}
 
 	/**
-	 * This returns the label text for {@link org.eclipse.emf.edit.command.CreateChildCommand}.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns the label text for
+	 * {@link org.eclipse.emf.edit.command.CreateChildCommand}. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
-	public String getCreateChildText(Object owner, Object feature, Object child, Collection<?> selection)
+	public String getCreateChildText(final Object owner, final Object feature, final Object child, final Collection<?> selection)
 	{
-		Object childFeature = feature;
-		Object childObject = child;
+		final Object childFeature = feature;
+		final Object childObject = child;
 
-		boolean qualify = childFeature == ExecutionTracePackage.Literals.ACTIVITY_EXECUTION__IN_PARAMETER_VALUES
+		final boolean qualify = childFeature == ExecutionTracePackage.Literals.ACTIVITY_EXECUTION__IN_PARAMETER_VALUES
 				|| childFeature == ExecutionTracePackage.Literals.ACTIVITY_EXECUTION__OUT_PARAMETER_VALUES;
 
 		if (qualify)
 		{
-			return getString("_UI_CreateChild_text2", new Object[]
+			return this.getString("_UI_CreateChild_text2", new Object[]
 			{
-					getTypeText(childObject), getFeatureText(childFeature), getTypeText(owner)
+					this.getTypeText(childObject), this.getFeatureText(childFeature), this.getTypeText(owner)
 			});
 		}
 		return super.getCreateChildText(owner, feature, child, selection);
 	}
-
 }
--- plugins/de.mdelab.sdm.interpreter.core.executionTrace.edit/src-gen/de/mdelab/sdm/interpreter/core/executionTrace/provider/ActivityNodeExecutionItemProvider.java
+++ plugins/de.mdelab.sdm.interpreter.core.executionTrace.edit/src-gen/de/mdelab/sdm/interpreter/core/executionTrace/provider/ActivityNodeExecutionItemProvider.java
@@ -2,16 +2,15 @@
  */
 package de.mdelab.sdm.interpreter.core.executionTrace.provider;
 
-import de.mdelab.sdm.interpreter.core.executionTrace.ActivityNodeExecution;
-import de.mdelab.sdm.interpreter.core.executionTrace.ExecutionTracePackage;
-
 import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.EMFEditPlugin;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemColorProvider;
 import org.eclipse.emf.edit.provider.IItemFontProvider;
@@ -25,10 +24,14 @@ import org.eclipse.emf.edit.provider.ITableItemLabelProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.eclipse.emf.edit.provider.ViewerNotification;
 
+import de.mdelab.sdm.interpreter.core.executionTrace.ActivityNodeExecution;
+import de.mdelab.sdm.interpreter.core.executionTrace.ExecutionTracePackage;
+
 /**
- * This is the item provider adapter for a {@link de.mdelab.sdm.interpreter.core.executionTrace.ActivityNodeExecution} object.
- * <!-- begin-user-doc -->
- * <!-- end-user-doc -->
+ * This is the item provider adapter for a
+ * {@link de.mdelab.sdm.interpreter.core.executionTrace.ActivityNodeExecution}
+ * object. <!-- begin-user-doc --> <!-- end-user-doc -->
+ * 
  * @generated
  */
 public class ActivityNodeExecutionItemProvider extends ExecutionItemProvider implements IEditingDomainItemProvider,
@@ -36,106 +39,121 @@ public class ActivityNodeExecutionItemProvider extends ExecutionItemProvider imp
 		ITableItemColorProvider, ITableItemFontProvider, IItemColorProvider, IItemFontProvider
 {
 	/**
-	 * This constructs an instance from a factory and a notifier.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This constructs an instance from a factory and a notifier. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
-	public ActivityNodeExecutionItemProvider(AdapterFactory adapterFactory)
+	public ActivityNodeExecutionItemProvider(final AdapterFactory adapterFactory)
 	{
 		super(adapterFactory);
 	}
 
 	/**
-	 * This returns the property descriptors for the adapted class.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns the property descriptors for the adapted class. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
-	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object)
+	public List<IItemPropertyDescriptor> getPropertyDescriptors(final Object object)
 	{
-		if (itemPropertyDescriptors == null)
+		if (this.itemPropertyDescriptors == null)
 		{
 			super.getPropertyDescriptors(object);
 
-			addActivityNodePropertyDescriptor(object);
+			this.addActivityNodePropertyDescriptor(object);
 		}
-		return itemPropertyDescriptors;
+		return this.itemPropertyDescriptors;
 	}
 
 	/**
-	 * This adds a property descriptor for the Activity Node feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This adds a property descriptor for the Activity Node feature. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
-	protected void addActivityNodePropertyDescriptor(Object object)
+	protected void addActivityNodePropertyDescriptor(final Object object)
 	{
-		itemPropertyDescriptors.add(createItemPropertyDescriptor(
-				((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
-				getResourceLocator(),
-				getString("_UI_ActivityNodeExecution_activityNode_feature"),
-				getString("_UI_PropertyDescriptor_description", "_UI_ActivityNodeExecution_activityNode_feature",
+		this.itemPropertyDescriptors.add(this.createItemPropertyDescriptor(((ComposeableAdapterFactory) this.adapterFactory)
+				.getRootAdapterFactory(), this.getResourceLocator(), this.getString("_UI_ActivityNodeExecution_activityNode_feature"), this
+				.getString("_UI_PropertyDescriptor_description", "_UI_ActivityNodeExecution_activityNode_feature",
 						"_UI_ActivityNodeExecution_type"), ExecutionTracePackage.Literals.ACTIVITY_NODE_EXECUTION__ACTIVITY_NODE, true,
 				false, true, null, null, null));
 	}
 
 	/**
-	 * This returns ActivityNodeExecution.gif.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns ActivityNodeExecution.gif. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
-	public Object getImage(Object object)
+	public Object getImage(final Object object)
 	{
-		return overlayImage(object, getResourceLocator().getImage("full/obj16/ActivityNodeExecution"));
+		return this.overlayImage(object, this.getResourceLocator().getImage("full/obj16/ActivityNodeExecution"));
 	}
 
 	/**
-	 * This returns the label text for the adapted class.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
+	 * This returns the label text for the adapted class. <!-- begin-user-doc
+	 * --> <!-- end-user-doc -->
+	 * 
+	 * @generated not
 	 */
 	@Override
-	public String getText(Object object)
+	public String getText(final Object object)
 	{
-		ActivityNodeExecution<?> activityNodeExecution = (ActivityNodeExecution<?>) object;
-		return getString("_UI_ActivityNodeExecution_type") + " " + activityNodeExecution.getExecutionStartedTimeStamp();
+		final ActivityNodeExecution<?> activityNodeExecution = (ActivityNodeExecution<?>) object;
+		String label = this.getString("_UI_ActivityNodeExecution_type");
+
+		label += " " + activityNodeExecution.getExecutionTimeMsec() + " msec - ";
+
+		final ComposedAdapterFactory factory = new ComposedAdapterFactory(EMFEditPlugin.getComposedAdapterFactoryDescriptorRegistry());
+
+		if (activityNodeExecution.getActivityNode() instanceof EObject)
+		{
+			label += ((IItemLabelProvider) factory.adapt(activityNodeExecution.getActivityNode(), IItemLabelProvider.class))
+					.getText(activityNodeExecution.getActivityNode());
+		}
+		else
+		{
+			label += activityNodeExecution.getActivityNode().toString();
+		}
+
+		return label;
 	}
 
 	/**
-	 * This handles model notifications by calling {@link #updateChildren} to update any cached
-	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This handles model notifications by calling {@link #updateChildren} to
+	 * update any cached children and by creating a viewer notification, which
+	 * it passes to {@link #fireNotifyChanged}. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
-	public void notifyChanged(Notification notification)
+	public void notifyChanged(final Notification notification)
 	{
-		updateChildren(notification);
+		this.updateChildren(notification);
 
 		switch (notification.getFeatureID(ActivityNodeExecution.class))
 		{
 			case ExecutionTracePackage.ACTIVITY_NODE_EXECUTION__ACTIVITY_NODE:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+				this.fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
 				return;
 		}
 		super.notifyChanged(notification);
 	}
 
 	/**
-	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
-	 * that can be created under this object.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s
+	 * describing the children that can be created under this object. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
-	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object)
+	protected void collectNewChildDescriptors(final Collection<Object> newChildDescriptors, final Object object)
 	{
 		super.collectNewChildDescriptors(newChildDescriptors, object);
 	}
