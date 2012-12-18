9175723890bf10ff3a0b6f31b9363a8eed3bbc13
chris227 <chris227@upb.de>
2012-12-18 17:02:59 +0000

    increase nsURI of modelinstance from 0.3.7 to 0.3.8

--- plugins/de.fujaba.modelinstance/src-gen/de/fujaba/modelinstance/impl/ModelinstanceFactoryImpl.java
+++ plugins/de.fujaba.modelinstance/src-gen/de/fujaba/modelinstance/impl/ModelinstanceFactoryImpl.java
@@ -34,7 +34,7 @@ public class ModelinstanceFactoryImpl extends EFactoryImpl implements Modelinsta
 	 */
 	public static ModelinstanceFactory init() {
 		try {
-			ModelinstanceFactory theModelinstanceFactory = (ModelinstanceFactory)EPackage.Registry.INSTANCE.getEFactory("http://www.fujaba.de/modelinstance/0.3.7"); 
+			ModelinstanceFactory theModelinstanceFactory = (ModelinstanceFactory)EPackage.Registry.INSTANCE.getEFactory("http://www.fujaba.de/modelinstance/0.3.8"); 
 			if (theModelinstanceFactory != null) {
 				return theModelinstanceFactory;
 			}
