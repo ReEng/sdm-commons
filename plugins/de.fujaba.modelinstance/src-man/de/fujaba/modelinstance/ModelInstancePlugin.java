cdca6d850916611965f2ccabeb0a1dfc823518c1
ingo.budde <ingo.budde@7db10e64-7fa7-f9b4-881c-49d84ee3ce2a>
2013-01-24 20:43:55 +0000

    Muml bug #439: Added extension point de.fujaba.modelinstance.version which allows to specify the versions of artefacts (also svn revision) and is used by the modelinstance resource to generate a comment in the fujaba file.

--- plugins/de.fujaba.modelinstance/src-man/de/fujaba/modelinstance/ModelInstancePlugin.java
+++ plugins/de.fujaba.modelinstance/src-man/de/fujaba/modelinstance/ModelInstancePlugin.java
@@ -1,5 +1,7 @@
 package de.fujaba.modelinstance;
 
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -10,7 +12,9 @@ public class ModelInstancePlugin implements BundleActivator {
 	private static ModelInstancePlugin instance;
 	private static BundleContext context;
 	private ModelElementCategoryRegistry modelElementCategoryRegistry;
-
+	
+	private String versionInformation;
+	
 	static BundleContext getContext() {
 		return context;
 	}
@@ -28,6 +32,7 @@ public class ModelInstancePlugin implements BundleActivator {
 		ModelInstancePlugin.context = bundleContext;
 		
 		modelElementCategoryRegistry = new ModelElementCategoryRegistry(true);
+		versionInformation = buildVersionInformation();
 	}
 
 	/*
@@ -38,11 +43,59 @@ public class ModelInstancePlugin implements BundleActivator {
 		ModelInstancePlugin.instance = null;
 		ModelInstancePlugin.context = null;
 		modelElementCategoryRegistry = null;
+		versionInformation = null;
 	}
 	
 
 	public ModelElementCategoryRegistry getModelElementCategoryRegistry() {
 		return modelElementCategoryRegistry;
+	}
+	
+
+	public static String getVersionInformation() {
+		return instance.versionInformation;
+	}
+
+	
+	
+	private String buildVersionInformation() {
+		StringBuilder builder = new StringBuilder();
+		IConfigurationElement[] topElements = Platform.getExtensionRegistry()
+				.getConfigurationElementsFor(
+						"de.fujaba.modelinstance.version");
+
+		for (IConfigurationElement artefact : topElements) {
+			String name = artefact.getAttribute("name");
+			String version = artefact.getAttribute("version");
+			builder.append(name);
+			builder.append(" version ");
+			builder.append(version);
+			for (IConfigurationElement change : artefact.getChildren()) {
+//				String committer = trim(change.getAttribute("committer"), "LastChangedBy:");
+//				String date = trim(change.getAttribute("date"), "LastChangedDate:");
+				String revision = trim(change.getAttribute("revision"), "LastChangedRevision:");
+				builder.append(" (");
+				builder.append('r');
+				builder.append(revision);
+				builder.append(')');
+			}
+		}
+
+		return builder.toString();
+	}
+
+	private String trim(String str, String keyword) {
+		if (str.charAt(0) == '$') {
+			str = str.substring(1);
+		}
+		if (str.charAt(str.length()-1) =='$') {
+			str = str.substring(0, str.length() - 2);
+		}
+		str = str.trim();
+		if (str.startsWith(keyword)) {
+			str = str.substring(keyword.length());
+		}
+		return str.trim();
 	}
 
 }
