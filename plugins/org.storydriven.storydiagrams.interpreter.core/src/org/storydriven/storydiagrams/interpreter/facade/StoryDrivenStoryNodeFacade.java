e6f758bda053ed484075a98f3fc1bae20b44879f
aljoschability <aljoschability@gmail.com>
2013-02-04 23:50:53 +0000

    organized meta data of most org.storydriven.* projects set all versions to 1.0.1.qualifier prepared POMs for maven build added composite update site including the MDElab dependencies

---
+++ plugins/org.storydriven.storydiagrams.interpreter.core/src/org/storydriven/storydiagrams/interpreter/facade/StoryDrivenStoryNodeFacade.java
@@ -0,0 +1,100 @@
+package org.storydriven.storydiagrams.interpreter.facade;
+
+import org.storydriven.storydiagrams.activities.ActivityEdge;
+import org.storydriven.storydiagrams.activities.ActivityNode;
+import org.storydriven.storydiagrams.activities.StoryNode;
+import org.storydriven.storydiagrams.patterns.StoryPattern;
+
+import de.mdelab.sdm.interpreter.core.facade.EForEachSemantics;
+import de.mdelab.sdm.interpreter.core.facade.IStoryNodeFacade;
+
+public class StoryDrivenStoryNodeFacade extends StoryDrivenActivityNodeFacade implements
+		IStoryNodeFacade<ActivityNode, ActivityEdge, StoryPattern>
+{
+
+	@Override
+	public StoryPattern getStoryPattern(ActivityNode activityNode)
+	{
+		assert activityNode != null;
+		assert activityNode instanceof StoryNode;
+
+		return ((StoryNode) activityNode).getStoryPattern();
+	}
+
+	@Override
+	public EForEachSemantics getForEachSemantics(ActivityNode activityNode)
+	{
+		assert activityNode != null;
+		assert activityNode instanceof StoryNode;
+
+		if (((StoryNode) activityNode).isForEach())
+		{
+			return EForEachSemantics.FRESH_MATCH;
+		}
+		else
+		{
+			return EForEachSemantics.SINGLE_MATCH;
+		}
+	}
+
+	@Override
+	public ActivityEdge getSuccessNextEdge(ActivityNode activityNode)
+	{
+		assert activityNode != null;
+		assert activityNode instanceof StoryNode;
+		assert activityNode.getOutgoings().size() >= 1;
+		assert activityNode.getOutgoings().size() <= 2;
+
+		if (activityNode.getOutgoings().size() == 1)
+		{
+			return activityNode.getOutgoings().get(0);
+		}
+		else
+		{
+			for (ActivityEdge edge : activityNode.getOutgoings())
+			{
+				switch (edge.getGuard())
+				{
+					case SUCCESS:
+					case EACH_TIME:
+						return edge;
+					default:
+						return null;
+				}
+			}
+
+			return null;
+		}
+	}
+
+	@Override
+	public ActivityEdge getFailureNextEdge(ActivityNode activityNode)
+	{
+		assert activityNode != null;
+		assert activityNode instanceof StoryNode;
+		assert activityNode.getOutgoings().size() >= 1;
+		assert activityNode.getOutgoings().size() <= 2;
+
+		if (activityNode.getOutgoings().size() == 1)
+		{
+			return activityNode.getOutgoings().get(0);
+		}
+		else
+		{
+			for (ActivityEdge edge : activityNode.getOutgoings())
+			{
+				switch (edge.getGuard())
+				{
+					case FAILURE:
+					case END:
+						return edge;
+					default:
+						return null;
+				}
+			}
+
+			return null;
+		}
+	}
+
+}
---
+++ plugins/org.storydriven.storydiagrams.interpreter.core/src/org/storydriven/storydiagrams/interpreter/facade/StoryDrivenStoryPatternFacade.java
@@ -0,0 +1,54 @@
+package org.storydriven.storydiagrams.interpreter.facade;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.storydriven.core.expressions.Expression;
+import org.storydriven.storydiagrams.activities.StoryNode;
+import org.storydriven.storydiagrams.patterns.AbstractLinkVariable;
+import org.storydriven.storydiagrams.patterns.AbstractVariable;
+import org.storydriven.storydiagrams.patterns.Constraint;
+import org.storydriven.storydiagrams.patterns.StoryPattern;
+
+import de.mdelab.sdm.interpreter.core.facade.IStoryPatternFacade;
+
+public class StoryDrivenStoryPatternFacade implements IStoryPatternFacade<StoryPattern, AbstractVariable, AbstractLinkVariable, Expression>
+{
+
+	@Override
+	public Collection<AbstractVariable> getStoryPatternObjects(StoryPattern sp)
+	{
+		assert sp != null;
+
+		return sp.getVariables();
+	}
+
+	@Override
+	public String getName(StoryPattern sp)
+	{
+		assert sp != null;
+
+		if (sp.eContainer() instanceof StoryNode)
+		{
+			return ((StoryNode) sp.eContainer()).getName();
+		}
+
+		return null;
+	}
+
+	@Override
+	public Collection<Expression> getConstraints(StoryPattern sp)
+	{
+		assert sp != null;
+
+		Collection<Expression> constraints = new ArrayList<Expression>(sp.getConstraints().size());
+
+		for (Constraint constraint : sp.getConstraints())
+		{
+			constraints.add(constraint.getConstraintExpression());
+		}
+
+		return constraints;
+	}
+
+}
