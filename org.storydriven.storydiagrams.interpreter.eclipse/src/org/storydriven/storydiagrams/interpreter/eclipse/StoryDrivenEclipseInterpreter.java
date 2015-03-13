42d962e58fbe20b8d7477c416fd53139f17b36bf
travkin <travkin@upb.de>
2015-03-13 21:47:13 +0000

    Tagged the last version known to be compatible to my POSE plug-ins, version from december 2012 (several changes came afterwards which may require POSE to be adapted)

---
+++ org.storydriven.storydiagrams.interpreter.eclipse/src/org/storydriven/storydiagrams/interpreter/eclipse/StoryDrivenEclipseInterpreter.java
@@ -0,0 +1,38 @@
+package org.storydriven.storydiagrams.interpreter.eclipse;
+
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.storydriven.core.expressions.Expression;
+import org.storydriven.storydiagrams.activities.Activity;
+import org.storydriven.storydiagrams.activities.ActivityEdge;
+import org.storydriven.storydiagrams.activities.ActivityNode;
+import org.storydriven.storydiagrams.interpreter.StoryDrivenInterpreter;
+import org.storydriven.storydiagrams.interpreter.facade.StoryDrivenMetamodelFacadeFactory;
+import org.storydriven.storydiagrams.patterns.AbstractLinkVariable;
+import org.storydriven.storydiagrams.patterns.AbstractVariable;
+import org.storydriven.storydiagrams.patterns.StoryPattern;
+
+import de.mdelab.sdm.interpreter.core.SDMException;
+import de.mdelab.sdm.interpreter.core.eclipse.EclipseExpressionInterpreterManager;
+import de.mdelab.sdm.interpreter.core.notifications.NotificationEmitter;
+
+public class StoryDrivenEclipseInterpreter extends StoryDrivenInterpreter
+{
+	public StoryDrivenEclipseInterpreter(
+
+			ClassLoader classLoader,
+			NotificationEmitter<Activity, ActivityNode, ActivityEdge, StoryPattern, AbstractVariable, AbstractLinkVariable, EClassifier, EStructuralFeature, Expression> notificationEmitter)
+			throws SDMException
+	{
+		super(
+				new EclipseExpressionInterpreterManager<Activity, ActivityNode, ActivityEdge, StoryPattern, AbstractVariable, AbstractLinkVariable, EClassifier, EStructuralFeature, Expression>(
+						StoryDrivenMetamodelFacadeFactory.INSTANCE, classLoader, notificationEmitter), notificationEmitter);
+	}
+
+	public StoryDrivenEclipseInterpreter(ClassLoader classLoader) throws SDMException
+	{
+		this(
+				classLoader,
+				new NotificationEmitter<Activity, ActivityNode, ActivityEdge, StoryPattern, AbstractVariable, AbstractLinkVariable, EClassifier, EStructuralFeature, Expression>());
+	}
+}
