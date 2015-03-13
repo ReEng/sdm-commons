b26371774c29b463959acfea51808507e281a7ff
travkin <travkin@upb.de>
2015-03-13 21:43:05 +0000

    Tagged the last version known to be compatible to my POSE plug-ins, version from december 2012 (several changes came afterwards which may require POSE to be adapted)

---
+++ org.storydriven.storydiagrams.expressions.common/src/org/storydriven/storydiagrams/expressions/common/ExpressionsStandaloneSetup.java
@@ -0,0 +1,15 @@
+
+package org.storydriven.storydiagrams.expressions.common;
+
+
+/**
+ * Initialization support for running Xtext languages 
+ * without equinox extension registry
+ */
+public class ExpressionsStandaloneSetup extends ExpressionsStandaloneSetupGenerated{
+
+	public static void doSetup() {
+		new ExpressionsStandaloneSetup().createInjectorAndDoEMFRegistration();
+	}
+}
+
---
+++ org.storydriven.storydiagrams.expressions.common/src/org/storydriven/storydiagrams/expressions/common/validation/ExpressionsJavaValidator.java
@@ -0,0 +1,14 @@
+package org.storydriven.storydiagrams.expressions.common.validation;
+
+ 
+
+public class ExpressionsJavaValidator extends AbstractExpressionsJavaValidator {
+
+//	@Check
+//	public void checkGreetingStartsWithCapital(Greeting greeting) {
+//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
+//			warning("Name should start with a capital", MyDslPackage.GREETING__NAME);
+//		}
+//	}
+
+}
